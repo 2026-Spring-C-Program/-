638cb2a011f1bfa99782374b28c8f5d06222cd96
luffyj5537-afk <luffyj5537@gmail.com>
2026-03-15 22:06:56 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Name          : 이윤정\n");
+    printf("Student ID    : 202610566\n");
+    printf("Major         : AI 빅데이터\n");
+    printf("Determination : C 언어를 능숙하게 사용하고 싶습니다!\n");
+    
+    return 0;
+}
+
